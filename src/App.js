a9f8cca8b97f1e2f49e4b9a6027f1cd61c63bdc5
akhegade <anilhegde006@gmail.com>
2021-04-25 18:00:33 +0530

    creating chat layout and linking it chatEngine.io project

--- src/App.js
+++ src/App.js
@@ -1,7 +1,14 @@
 import React from "react";
+import {ChatEngine} from "react-chat-engine";
+import "./App.css";
 
 const App = () => {
-  return <div>Starting.....chat_application</div>;
+  return 	<ChatEngine
+			height='100vh'
+			userName='anil'
+			userSecret='anil@chat1'
+			projectID='d8dc4ff3-55df-4965-b1ac-58b5beafd5b5'
+		 />;
 };
 
 export default App;
